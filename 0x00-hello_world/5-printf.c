fc2f3e0d97e9b16463e03c615603ef5df08cf4d5
Michelle-chats <mtchatikobo@gmail.com>
2022-07-01 14:06:37 -0700

    submitting

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - prints exactly with proper grammar,but the outcome is a piece of art
- * your program should return 0
- * return: 0 if exited properly , non-zero otherwise
+ * main - prints Use the function printf
+ * Your program should return 0
+ * Return: 0 if exited propery , non-zero otherwise
  */
 int main(void)
 {
