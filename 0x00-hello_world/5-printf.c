e4f3985eb61fb2a387c3024249bb7680628411d1
Michelle-chats <mtchatikobo@gmail.com>
2022-07-01 14:03:59 -0700

    submitting

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints exactly with proper grammar,but the outcome is a piece of art
+ * your program should return 0
+ * return: 0 if exited properly , non-zero otherwise
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
