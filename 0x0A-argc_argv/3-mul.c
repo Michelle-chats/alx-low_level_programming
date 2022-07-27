adf3b239b5e422f6cb2deb8c8c04f2acde43cd8f
Michelle-chats <mtchatikobo@gmail.com>
2022-07-27 13:18:49 -0700

    submitting

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -  multiplies two numbers.
+ * @argc: tamaño de argv
+ *  @argv: program
+ *   Return: value 0
+ */
+int main(int argc, char *argv[])
+{
+	int multple;
+
+	if (argc == 3)
+	{
+		multple = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", multple);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
