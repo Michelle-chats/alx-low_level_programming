ac575be2bef9336ea5dc138f78a5334488efc3e6
Michelle-chats <mtchatikobo@gmail.com>
2022-08-02 02:39:33 -0700

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef STRTYP
+#define STRTYP
 
 /**
  * struct dog - a structur with arguments for it's name, age, and owner
@@ -13,9 +13,12 @@ typedef struct dog
 	float age;
 	char *owner;
 };
+
 typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-#endif
+
+#endif /*STRTYP*/
