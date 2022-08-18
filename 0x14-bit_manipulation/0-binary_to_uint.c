9ee0a274ef7258305b724c1434d0414890d5a5a0
Michelle-chats <mtchatikobo@gmail.com>
2022-08-18 12:58:07 -0700

    ok

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number to unsigned int
+ * @b: string containing the binary number
+ *
+ * Return: the converted number
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int var = 1, current = 0, count = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while (b[count])
+		count++;
+
+	while (count)
+	{
+		if (b[count - 1] != '0' && b[count -1] != '1')
+			return (0);
+		var *= 2;
+		count --;
+	}
+	return (current);
+
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,1 @@
+This is a README File for assignment 14 Bit manipulation.
