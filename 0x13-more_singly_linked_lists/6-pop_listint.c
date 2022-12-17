ba097f9997a25f9f985779f4270cf68ccafc773b
Michelle-chats <mtchatikobo@gmail.com>
2022-12-17 12:46:21 -0800

    submitting

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * pop_listint - function that deletes head node of a listint_t linked list,
+ * and returns the head node’s data (n).
+ * @head: A pointer to listint_t structure.
+ * Return: The head node's datafrom deleted element
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *NewNode;
+	int n;
+
+	if (*head == NULL)
+		return (0);
+
+	n = (*head)->n;
+	NewNode = (*head)->next;
+	free(*head);
+	*head = NewNode;
+
+	return (n);
+}
---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,53 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - inserts a new node in a linked list,
+ * at a given position
+ * @head: pointer to the first node in the list
+ * @idx: index where the new node is added
+ * @n: data to insert in the new node
+ *
+ * Return: pointer to the new node, or NULL
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new;
+	listint_t *current;
+	unsigned int count;
+
+	if (head == NULL)
+		return (NULL);
+
+	current = *head;
+	for (count = 1; current && count < index; count++)
+	{
+		current = current->next
+		if (current == NULL)
+			return (NULL);
+	}
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->n = n;
+
+	if (index == 0)
+	{
+		*head = new;
+		new->next = current;
+	}
+	else if (current->next)
+	{
+		new->next = current->next;
+		current->next = new;
+	}
+	else
+	{
+		new->next = NULL;
+		current->next = new;
+	}
+	return (new);
+}
