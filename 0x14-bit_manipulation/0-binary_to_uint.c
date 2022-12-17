cd96fb0f1744a43f805a10c16204f8494d4e9532
Michelle-chats <mtchatikobo@gmail.com>
2022-12-17 11:32:54 -0800

    submitting

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -4,27 +4,29 @@
 /**
  * binary_to_uint - converts a binary number to unsigned int
  * @b: char string
- * Return: the converted number
+ * Return: the converted number 0 if there are characters that are not 0 or 1
+ * or b is NULL
  */
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int total, power;
-int len;
+	unsigned int result = 0;
+	unsigned int i;
 
-if (b == NULL)
-	return (0);
+	if (b == NULL)
+		return (0)
 
-for (len = 0; b[len]; len++)
-{
-	if (b[len] != '0' && b[len] != '1')
-		return (0);
-}
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+	}
 
-for (power = 1, total = 0, len--; len >= 0; len--, power *= 2)
-{
-	if (b[len] == '1')
-		total += power;
-}
+	for (i = 0; b[i] == '0' || b[i] == '1'; i++)
+	{
+		result = result << 1; /* multiplies by two */
+		if (b[i] == '1')
+			result++;
+	}
 
-return (total);
+	return (result);
 }
