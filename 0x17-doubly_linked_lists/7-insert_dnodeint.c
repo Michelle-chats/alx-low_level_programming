6d522e35d04828e16f07b89bfb786e32b6ee72d1
Michelle-chats <you@example.com>
2023-05-08 16:28:46 +0200

    submitting

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -3,97 +3,47 @@
 #include "lists.h"
 
 /**
-  * delete_dnodeint_at_index - ...
-  * @head: ...
-  * @index: ...
+  * insert_dnodeint_at_index - Inserts a new node at a given position
+  * @h: The head of the doubly linked list
+  * @idx: The index in which insert the new node
+  * @n: The number to insert in the new node
   *
-  * Return: ...
+  * Return: The address of the new node, or NULL if it failed
   */
-int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *current = NULL, *temp = NULL;
-	unsigned int length = 0;
+	dlistint_t *current = NULL, *new_node = NULL;
+	unsigned int iter_times = 0, length = 0;
 
-	if (head && *head)
+	if (h == NULL)
+		return (NULL);
+
+	if (*h == NULL && idx == 0)
+		return (add_dnodeint(h, n));
+
+	length = dlistint_len(*h);
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+	else if (length == idx)
+		return (add_dnodeint_end(h, n));
+
+	current = *h;
+	while (current != NULL)
 	{
-		length = dlistint_len(*head);
-		if (index > length)
-			return (-1);
-
-		if (index == 0)
-			return (delete_first_dnode(head));
-
-		current = get_dnodeint_at_index(*head, index);
-		if (current)
+		if (iter_times == idx)
 		{
-			temp = current;
-			if (length - 1 == index)
-				current->prev->next = current->next;
-			else
-			{
-				current->prev->next = current->next;
-				current->next->prev = current->prev;
-			}
-
-			free(temp);
-			return (1);
+			new_node = create_node(n, current, current->prev);
+			current->prev = new_node;
+			current = new_node;
+			current->prev->next = new_node;
+			return (new_node);
 		}
-	}
 
-	return (-1);
-}
-
-/**
-  * delete_first_dnode - Remove the first node of a doubly linked list
-  * @head: The head of the doubly linked list
-  *
-  * Return: 1 if is deleted
-  */
-int delete_first_dnode(dlistint_t **head)
-{
-	dlistint_t *current = *head, *temp = NULL;
-
-	temp = current;
-	if (current->next)
-	{
 		current = current->next;
-		current->prev = temp->prev;
-		*head = current;
-	}
-	else
-	{
-		*head = NULL;
+		++iter_times;
 	}
 
-	free(temp);
-	return (1);
-}
-
-/**
-  * get_dnodeint_at_index - Gets a node from a doubly linked list
-  * @head: The head of the doubly linked list
-  * @index: The index to find in the doubly linked list
-  *
-  * Return: The specific node of the doubly linked list
-  */
-dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
-{
-	dlistint_t *current = head;
-	unsigned int iter_times = 0;
-
-	if (head)
-	{
-		while (current != NULL)
-		{
-			if (iter_times == index)
-				return (current);
-
-			current = current->next;
-			++iter_times;
-		}
-	}
-
-	return (NULL);
+	return (current);
 }
 
 /**
@@ -113,4 +63,26 @@ size_t dlistint_len(const dlistint_t *h)
 	}
 
 	return (lenght);
+}
+
+/**
+  * create_node - Create a new node with values
+  * @n: The number of the new node
+  * @next: The next node of the new node
+  * @prev: The previous node of the new node
+  *
+  * Return: The address of the new node created
+  */
+dlistint_t *create_node(unsigned int n, void *next, void *prev)
+{
+	dlistint_t *new_node = NULL;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = next;
+	new_node->prev = prev;
+	return (new_node);
 }
